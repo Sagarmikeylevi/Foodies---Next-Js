2a778966e64a4aaba2166aeb96e5473a050006bb
unknown <sagarmikeylevi@gmail.com>
2024-02-01 18:33:13 +0530

    handling errors

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,15 +1,37 @@
 import Link from "next/link";
 
-export default function Meals() {
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+import MealsLoadingPage from "./loading-out";
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals</h1>
-      <p>
-        <Link href="/meals/share">Share</Link>
-      </p>
-      <p>
-        <Link href="/meals/meal-1">Meal-1</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<MealsLoadingPage />}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
   );
 }
